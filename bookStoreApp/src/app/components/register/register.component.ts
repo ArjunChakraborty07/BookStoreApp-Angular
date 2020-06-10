40ce2642405ac2b7414f4f4a8fe2ccd95e683407
venkataganapathi <kandulaganesh0@gmail.com>
2020-06-10 19:33:43 +0530

    [Ganapathi] Refactored : user service import statement

--- bookStoreApp/src/app/components/register/register.component.ts
+++ bookStoreApp/src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormControl } from '@angular/forms';
-import { UserService } from 'src/app/services/user.service';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-register',
