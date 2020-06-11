099e9cce7bfb332e463ace4518c5d7a16499bfe8
datta235 <saidatta1432015@gmail.com>
2020-06-11 20:12:51 +0530

    [datta] refactored: refactored the code according to typescript coding conventions

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -1,43 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterComponent } from './components/register/register.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule} from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatRadioModule} from '@angular/material/radio';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RegisterComponent } from "./components/register/register.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatRadioModule } from "@angular/material/radio";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import {
   MatSnackBarModule,
   MatTooltipModule,
   MatMenuModule,
-  MatMenuTrigger,
   MatIconModule,
   MatButtonModule,
   MatExpansionModule,
   MatSidenavModule,
   MatListModule,
-  
-} from '@angular/material';
-import { HttpClientModule } from '@angular/common/http';
-import { UserService } from 'src/services/user.service';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { VerificationComponent } from './components/verification/verification.component';
+} from "@angular/material";
+import { HttpClientModule } from "@angular/common/http";
+import { UserService } from "src/services/user.service";
+import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
+import { ResetPasswordComponent } from "./components/reset-password/reset-password.component";
+import { VerificationComponent } from "./components/verification/verification.component";
 import { EncrDecrService } from "src/services/encr-decr.service";
 
-//import { UserService } from './services/user.service';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HttpService } from 'src/services/http.service';
-//import { EncrDecrService } from './services/encr-decr-service.service';
-import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HttpService } from "src/services/http.service";
+
+import { LoginComponent } from "./components/login/login.component";
 
 @NgModule({
   declarations: [
@@ -47,9 +43,8 @@ import { LoginComponent } from './components/login/login.component';
     ForgotPasswordComponent,
     ResetPasswordComponent,
     VerificationComponent,
-     LoginComponent,
+    LoginComponent,
     DashboardComponent,
-
   ],
   imports: [
     BrowserModule,
@@ -57,7 +52,8 @@ import { LoginComponent } from './components/login/login.component';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule, ReactiveFormsModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     MatCardModule,
     MatToolbarModule,
@@ -68,13 +64,13 @@ import { LoginComponent } from './components/login/login.component';
     MatExpansionModule,
     MatSidenavModule,
     MatListModule,
-    //MatMenuTrigger,
+
     MatSnackBarModule,
     MatRadioModule,
-    FlexLayoutModule
+    FlexLayoutModule,
   ],
- providers: [UserService, EncrDecrService, HttpService],
+  providers: [UserService, EncrDecrService, HttpService],
 
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
