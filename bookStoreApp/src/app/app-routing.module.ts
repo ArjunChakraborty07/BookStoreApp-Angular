770d250eca5f61fe03d6bb72034e78ec8610f083
venkataganapathi <kandulaganesh0@gmail.com>
2020-06-16 11:17:57 +0530

    [ganapathi] Resloved : merge conflicts

--- bookStoreApp/src/app/app-routing.module.ts
+++ bookStoreApp/src/app/app-routing.module.ts
@@ -4,21 +4,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { VerificationComponent } from './components/verification/verification.component';
-<<<<<<< HEAD
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LoginComponent } from './components/login/login.component';
-
-
-const routes: Routes = [
-{path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent },
-  {path: 'forgotpassword', component: ForgotPasswordComponent},
-  {path: 'verification/:token', component: VerificationComponent},
-  {path: 'resetpassword/:token', component: ResetPasswordComponent},
-=======
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { VerificationComponent } from './components/verification/verification.component';
 
 
 const routes: Routes = [
@@ -28,7 +15,6 @@ const routes: Routes = [
   { path: 'forgotpassword', component: ForgotPasswordComponent },
   { path: 'verification/:token', component: VerificationComponent },
   { path: 'resetpassword/:token', component: ResetPasswordComponent },
->>>>>>> 50360e3e63a357648c024b41cd525012840f1a5b
   {
     path: 'dashboard',
     component: DashboardComponent,
