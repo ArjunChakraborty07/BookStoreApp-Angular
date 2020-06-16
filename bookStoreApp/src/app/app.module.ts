edc4480050dae7388b9f78a05fcc3d70c73a3409
datta235 <saidatta1432015@gmail.com>
2020-06-16 14:52:14 +0530

    [datta] deleted: unwanted imports

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -1,37 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterComponent } from './components/register/register.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule} from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatRadioModule} from '@angular/material/radio';
-import { FlexLayoutModule } from '@angular/flex-layout'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RegisterComponent } from "./components/register/register.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatRadioModule } from "@angular/material/radio";
+import { FlexLayoutModule } from "@angular/flex-layout";
 import {
   MatSnackBarModule,
   MatTooltipModule,
   MatMenuModule,
-  MatMenuTrigger,
   MatIconModule,
   MatButtonModule,
   MatExpansionModule,
   MatSidenavModule,
   MatListModule,
-} from '@angular/material';
-import { HttpClientModule } from '@angular/common/http';
-import { UserService } from '../services/user.service';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HttpService } from 'src/services/http.service';
-import { MatDialogModule} from '@angular/material/dialog';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { LoginComponent } from './components/login/login.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { VerificationComponent } from './components/verification/verification.component';
-import { EncrDecrService } from 'src/services/encr-decr.service';
+} from "@angular/material";
+import { HttpClientModule } from "@angular/common/http";
+import { UserService } from "../services/user.service";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HttpService } from "src/services/http.service";
+import { MatDialogModule } from "@angular/material/dialog";
+import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
+import { LoginComponent } from "./components/login/login.component";
+import { ResetPasswordComponent } from "./components/reset-password/reset-password.component";
+import { VerificationComponent } from "./components/verification/verification.component";
+import { EncrDecrService } from "src/services/encr-decr.service";
+import { VendorDashboardComponent } from "./components/vendor-dashboard/vendor-dashboard.component";
 
 @NgModule({
   declarations: [
@@ -41,8 +41,8 @@ import { EncrDecrService } from 'src/services/encr-decr.service';
     ForgotPasswordComponent,
     LoginComponent,
     ResetPasswordComponent,
-    VerificationComponent
-
+    VerificationComponent,
+    VendorDashboardComponent,
   ],
   imports: [
     BrowserModule,
@@ -50,7 +50,8 @@ import { EncrDecrService } from 'src/services/encr-decr.service';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule, ReactiveFormsModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     MatCardModule,
     MatToolbarModule,
@@ -65,9 +66,9 @@ import { EncrDecrService } from 'src/services/encr-decr.service';
     MatSnackBarModule,
     MatRadioModule,
     MatDialogModule,
-    FlexLayoutModule
+    FlexLayoutModule,
   ],
   providers: [UserService, EncrDecrService, HttpService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
