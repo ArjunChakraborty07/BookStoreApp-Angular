ff7eddbe56b610c9d5e652c1a769ccdf56dee888
datta235 <saidatta1432015@gmail.com>
2020-06-16 14:51:33 +0530

    [datta] added: routing for vendor-dashboard

--- bookStoreApp/src/app/app-routing.module.ts
+++ bookStoreApp/src/app/app-routing.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { RegisterComponent } from './components/register/register.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LoginComponent } from './components/login/login.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { VerificationComponent } from './components/verification/verification.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RegisterComponent } from "./components/register/register.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { LoginComponent } from "./components/login/login.component";
+import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
+import { VerificationComponent } from "./components/verification/verification.component";
+import { ResetPasswordComponent } from "./components/reset-password/reset-password.component";
+import { VendorDashboardComponent } from "./components/vendor-dashboard/vendor-dashboard.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'forgotpassword', component: ForgotPasswordComponent },
-  { path: 'verification/:token', component: VerificationComponent },
-  { path: 'resetpassword/:token', component: ResetPasswordComponent },
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: "forgotpassword", component: ForgotPasswordComponent },
+  { path: "verification/:token", component: VerificationComponent },
+  { path: "resetpassword/:token", component: ResetPasswordComponent },
   {
-    path: 'dashboard',
+    path: "dashboard",
     component: DashboardComponent,
+    children: [],
+  },
+  {
+    path: "vendor-dashboard",
+    component: VendorDashboardComponent,
     children: [],
   },
 ];
@@ -26,6 +31,12 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
-export const routingComponents = [RegisterComponent, DashboardComponent, LoginComponent,
-  ForgotPasswordComponent, VerificationComponent, ResetPasswordComponent];
+export class AppRoutingModule {}
+export const routingComponents = [
+  RegisterComponent,
+  DashboardComponent,
+  LoginComponent,
+  ForgotPasswordComponent,
+  VerificationComponent,
+  ResetPasswordComponent,
+];
