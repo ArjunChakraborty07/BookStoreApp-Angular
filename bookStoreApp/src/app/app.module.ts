b99f19cffb6966b9a737285104fb1fed944e0686
venkataganapathi <kandulaganesh0@gmail.com>
2020-06-16 11:18:10 +0530

    [ganapathi] Resloved : merge conflicts

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -23,7 +23,6 @@ import {
   MatListModule,
 } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
 import { UserService } from 'src/services/user.service';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
@@ -31,17 +30,15 @@ import { VerificationComponent } from './components/verification/verification.co
 import { EncrDecrService } from 'src/services/encr-decr.service';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HttpService } from 'src/services/http.service';
-=======
-import { UserService } from '../services/user.service';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HttpService } from 'src/services/http.service';
+// import { UserService } from '../services/user.service';
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
+// import { HttpService } from 'src/services/http.service';
 import { MatDialogModule} from '@angular/material/dialog';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
->>>>>>> 50360e3e63a357648c024b41cd525012840f1a5b
+// import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { LoginComponent } from './components/login/login.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { VerificationComponent } from './components/verification/verification.component';
-import { EncrDecrService } from 'src/services/encr-decr.service';
+// import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+// import { VerificationComponent } from './components/verification/verification.component';
+// import { EncrDecrService } from 'src/services/encr-decr.service';
 import { MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
