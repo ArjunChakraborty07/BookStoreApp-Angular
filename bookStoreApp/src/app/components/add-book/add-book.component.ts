e2645a267a85398633caa76857f4098d008cec4b
datta235 <saidatta1432015@gmail.com>
2020-06-26 00:14:40 +0530

    [datta] Added : add-book component

--- bookStoreApp/src/app/components/add-book/add-book.component.ts
+++ bookStoreApp/src/app/components/add-book/add-book.component.ts
@@ -11,6 +11,17 @@ import { Book } from 'src/models/book.model';
   styleUrls: ['./add-book.component.scss'],
 })
 export class AddBookComponent implements OnInit {
+  file: any;
+  isProfile = 'false';
+  bookImageUrl: any;
+  book = {
+    bookName: null,
+    authorName: null,
+    price: null,
+    quantity: null,
+    bookDetails: null,
+    image: null,
+  };
   constructor(
     private vendorService: VendorService,
     private snackBar: MatSnackBar,
@@ -25,15 +36,39 @@ export class AddBookComponent implements OnInit {
     price: new FormControl('', [Validators.min(1), Validators.required]),
     quantity: new FormControl('', [Validators.min(1), Validators.required]),
     bookDetails: new FormControl('', Validators.required),
-    image: new FormControl('', Validators.required),
+    image: new FormControl(this.bookImageUrl, Validators.required),
   });
   ngOnInit() {}
 
   onFormSubmit() {
     this.dialogRef.close();
-    this.vendorService.addBook(this.bookForm.value).subscribe((data) => {
-      console.log(data.data);
+    this.book.bookName = this.bookForm.value.bookName;
+    this.book.authorName = this.bookForm.value.authorName;
+    this.book.price = this.bookForm.value.price;
+    this.book.quantity = this.bookForm.value.quantity;
+    this.book.bookDetails = this.bookForm.value.bookDetails;
+    this.book.image = this.bookImageUrl;
+    console.log(this.book.image);
+
+    this.vendorService.addBook(this.book).subscribe((data) => {
       this.messageService.changeMessage();
     });
+  }
+  onUploadBookImage(event) {
+    if (event.target.files.length > 0) {
+      this.file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('file', this.file);
+      this.file.inProgress = true;
+      this.vendorService
+        .uploadBookImage(formData, this.isProfile)
+        .subscribe((data: any) => {
+          if (data.status === 200) {
+            this.bookImageUrl = data.data;
+            console.log(data);
+            console.log(this.bookImageUrl);
+          }
+        });
+    }
   }
 }
