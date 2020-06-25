5962141040ce48f67b733cb1259d5b26b9e2f512
datta235 <saidatta1432015@gmail.com>
2020-06-26 00:16:08 +0530

    [datta] Refactored : Added isProfile variable

--- bookStoreApp/src/app/components/dashboard/dashboard.component.ts
+++ bookStoreApp/src/app/components/dashboard/dashboard.component.ts
@@ -2,67 +2,65 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Observable, interval, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { DashboardService } from 'src/services/dashboard.service';
-import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { RegisterComponent } from '../register/register.component';
 import { LoginComponent } from '../login/login.component';
-import {EditProfileComponent} from '../edit-profile/edit-profile.component';
+import { EditProfileComponent } from '../edit-profile/edit-profile.component';
 import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
   data: any;
+  isProfile = 'true';
   searchBook: string;
   books: any;
-  profile:string;
-  login:boolean;
-  username:string;
-  usermail:string;
+  profile: string;
+  login: boolean;
+  username: string;
+  usermail: string;
   updateStats: any;
-  file:any;
-  constructor(private userService: UserService,
-              private service: DashboardService, 
-              private router: Router,
-              public dialog: MatDialog) 
-  {
-     if(localStorage.length===0)
-     {
-       this.login=false;
-       console.log("note logged");
-       this.profile='./assets/images/user.png';
-     }
-     else
-     {
-       console.log("logged in");
-       this.login=true;
-       this.username=localStorage.getItem('name');
-       this.usermail=localStorage.getItem('email');
-       if(localStorage.getItem('image').length!==0)
-       {
-          this.profile=localStorage.getItem("image");
-       }
-       if(localStorage['image']===undefined || localStorage['image']===0 || localStorage['image']===null || localStorage['image']===false || localStorage['image']==='')
-       {
-        console.log("image length",localStorage.getItem('image').length);
-        this.profile="./assets/images/user.png";
-       }
-         
-     }
+  file: any;
+  constructor(
+    private userService: UserService,
+    private service: DashboardService,
+    private router: Router,
+    public dialog: MatDialog
+  ) {
+    if (localStorage.length === 0) {
+      this.login = false;
+      console.log('note logged');
+      this.profile = './assets/images/user.png';
+    } else {
+      console.log('logged in');
+      this.login = true;
+      this.username = localStorage.getItem('name');
+      this.usermail = localStorage.getItem('email');
+      if (localStorage.getItem('image').length !== 0) {
+        this.profile = localStorage.getItem('image');
+      }
+      if (
+        localStorage['image'] === undefined ||
+        localStorage['image'] === 0 ||
+        localStorage['image'] === null ||
+        localStorage['image'] === false ||
+        localStorage['image'] === ''
+      ) {
+        console.log('image length', localStorage.getItem('image').length);
+        this.profile = './assets/images/user.png';
+      }
+    }
   }
-  openDialogztoedit()
-  {
-        this.dialog.open(EditProfileComponent);  
+  openDialogztoedit() {
+    this.dialog.open(EditProfileComponent);
   }
   openDialog(): void {
     const dialogRef = this.dialog.open(LoginComponent, {
       width: '40%',
-      height:'90%'
-      
-      
+      height: '90%',
     });
   }
   ngOnInit() {}
@@ -80,36 +78,33 @@ export class DashboardComponent implements OnInit {
   onLogin() {
     this.router.navigate(['/login']);
   }
-  onsignup()
-  {
+  onsignup() {
     this.router.navigate(['/register']);
   }
-  Logout()
-  {
-    console.log("CAME TO LOGOUT");
+  Logout() {
+    console.log('CAME TO LOGOUT');
     localStorage.clear();
     console.log(localStorage.length);
     this.router.navigate(['/dashboard']);
   }
-  OnSelectedFile(event)
-  {
+  OnSelectedFile(event) {
     console.log(event.target.files[0]);
-    if(event.target.files.length>0)
-    {
-      this.file=event.target.files[0];
-      const formData = new FormData();  
-      formData.append('file', this.file);  
+    if (event.target.files.length > 0) {
+      this.file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('file', this.file);
       this.file.inProgress = true;
-      console.log("FormData:",formData.get('file'));
-      this.userService.uploadProfie(formData).subscribe((result:any)=>{
-      console.log("PROFILE RESULT:",result);
-      if(result.status===200)
-      {
-        localStorage.setItem("image",result.data);
-        this.profile=result.data;
-        console.log(this.profile)
-      }
-      });
+      console.log('FormData:', formData.get('file'));
+      this.userService
+        .uploadProfie(formData, this.isProfile)
+        .subscribe((result: any) => {
+          console.log('PROFILE RESULT:', result);
+          if (result.status === 200) {
+            localStorage.setItem('image', result.data);
+            this.profile = result.data;
+            console.log(this.profile);
+          }
+        });
     }
   }
 }
