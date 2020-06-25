e92a67d10b6b9a3bb837e26b898093f8e0e8c442
datta235 <saidatta1432015@gmail.com>
2020-06-26 00:22:15 +0530

    [datta] Refactored : isProfile added  as request param

--- bookStoreApp/src/services/vendor.service.ts
+++ bookStoreApp/src/services/vendor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -12,32 +12,43 @@ export class VendorService {
   private updateBookApi = 'sellers/updateBook';
   private deleteBookApi = 'sellers/removeBook/';
   private displayBookApi = 'sellers/displayBooks/';
+  private uploadBookProfileApi = 'users/uploadImage';
 
   constructor(private http: HttpClient) {}
 
-  addBook(formGroup: FormGroup): Observable<any> {
-    return this.http.post(
-      environment.baseUrl + this.addBookApi,
-      formGroup,{
-         headers: new HttpHeaders().set('token', localStorage.getItem('token')),
-      }
-    );
+  addBook(book: any): Observable<any> {
+    return this.http.post(environment.baseUrl + this.addBookApi, book, {
+      headers: new HttpHeaders().set('token', localStorage.getItem('token')),
+    });
   }
 
   displayBooks(): Observable<any> {
-    return this.http.get(
-      environment.baseUrl + this.displayBookApi ,{
-         headers: new HttpHeaders().set('token', localStorage.getItem('token')),
-      }
-    );
+    return this.http.get(environment.baseUrl + this.displayBookApi, {
+      headers: new HttpHeaders().set('token', localStorage.getItem('token')),
+    });
   }
 
   deleteBooks(bookId: any): Observable<any> {
-    return this.http.delete(
-      environment.baseUrl +
-        this.deleteBookApi +
-        bookId ,{
-         headers: new HttpHeaders().set('token', localStorage.getItem('token')),
+    return this.http.delete(environment.baseUrl + this.deleteBookApi + bookId, {
+      headers: new HttpHeaders().set('token', localStorage.getItem('token')),
+    });
+  }
+  uploadBookImage(file: FormData, isProfile: string): Observable<any> {
+    return this.http.post(
+      environment.baseUrl + this.uploadBookProfileApi,
+      file,
+      {
+        headers: new HttpHeaders().set('token', localStorage.getItem('token')),
+        params: new HttpParams().set('isProfile', isProfile),
+      }
+    );
+  }
+  updateBook(formGroup: FormGroup, bookId: any): Observable<any> {
+    return this.http.put(
+      environment.baseUrl + this.updateBookApi + '/' + bookId,
+      formGroup,
+      {
+        headers: new HttpHeaders().set('token', localStorage.getItem('token')),
       }
     );
   }
